c82d1164c9586197f0042a5cddb8d396dcd5da94
raffaeleav <raff.aviello@gmail.com>
2026-04-03 14:10:33 +0200

    fix: detailed summary wasn't considering new changes

--- percival/core/rengine/write.py
+++ percival/core/rengine/write.py
@@ -225,13 +225,13 @@ def get_detailed_summary():
             "All the findings are organized in the findings.html file. "
             "This document provides a structured overview of all identified issues and the "
             "metadata collected during the analysis. "
-            "Package and language findings include CVSS scores (when available) for CVEs "
+            "Package and language findings include CVSS scores for CVEs "
             "associated with each package or dependency installed in the container image, "
-            "together with results derived from the Trivy scanner. "
+            "together with results derived from the Trivy scanner (if requested). "
             "Configuration findings highlight bad practices in Dockerfiles and include an "
             "efficiency assessment performed using the Dive tool. "
             "Secret detection findings report high-entropy strings and potential API keys. "
-            "You can use the fview command to quickly open and inspect the file in your web browser."
+            "You can use the 'findings' command to quickly open and inspect the file in your web browser."
         )
     ]
 
